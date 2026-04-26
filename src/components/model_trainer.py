93ceca5ee575f060fb2f6555638c0107a2bc169b
sahil patil <173548370+Sahilpatil009@users.noreply.github.com>
2026-04-26 16:47:34 +0530

    hyperparameter training add

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -29,7 +29,7 @@ class ModelTrainer:
 
     def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
         try:
-            logging.info("Split trainig and test input data")
+            logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
                 train_array[:, :-1],
                 train_array[:, -1],
@@ -48,8 +48,39 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
 
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "Random Forest": {
+                    'n_estimators': [8,16,32,64,128,256]
+                }, 
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [5,7,9,11],
+                    
+                },
+                "XGBRegressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'depth': [6,8,10],
+                    'iterations': [30,50,100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -23,11 +24,16 @@ def save_object(file_path: str, obj) -> None:
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
-        for i in range(len(models)):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            model_params = params.get(model_name, {})
+
+            gs=GridSearchCV(model, model_params, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -36,7 +42,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
